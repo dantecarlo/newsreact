6ca8d252aa1e578e87eedd0d5880766c5bf4e8b3
Gunbound.69 <>
2019-08-26 20:52:58 -0500

    ADD get news from an endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    news: [],
+  }
+
+  componentDidMount() {
+    this.getNews()
+  }
+
+  getNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e11830532bb1419cbc1de4f55d294626`
+    const res = await fetch(url)
+    const news = await res.json()
+
+    this.setState({ news: news.articles })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>API news React</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
