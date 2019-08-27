33c2af7c474c3a1a732f189bd4f75596d1c4d43f
dantecarlo <>
2019-08-27 10:46:20 -0500

    UPDATE interface names

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import News from './News'
 
 interface Props {
-  newsList: News[]
+  newsList: NewsI[]
 }
 
-interface News {
+interface NewsI {
   author: string
   content: string
   description: string
